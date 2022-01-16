ad3d6b4ded644a4787f1628d053c17143c3fbf7c
Eliran Eliassy <eliran.eliassy@gmail.com>
2022-01-16 10:06:43 +0200

    init workspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Item } from './item.interface';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'yotpo-course-project';
+
+  items: Item[] = [];
+  item1: Item = {
+    description: 'Playstation 5',
+    imageUrl: `https://d3m9l0v76dty0.cloudfront.net/system/photos/6207070/large/cd7c8f87e1dfc8f9e9d0bf50b738adaf.jpg`,
+    price: 500
+  };
+
+  item2: Item = {
+    description: 'Snoker table',
+    imageUrl: 'https://www.thailandpooltables.com/pub/media/catalog/product/cache/cd26d59ef39544881f47d6a7b57c93ba/k/e/kensington_10ft_snooker_table_-_thailand_pool_tables.jpg',
+    price: 3600
+  };
+
+  constructor() {
+    this.items = [this.item1, this.item2];
+  }
+
+
 }
+
+
+
